b37ff8731ce6ce188f04dabfcce9dcbad8a83933
kisekitw <kisekitw@gmail.com>
2018-07-30 21:35:24 +0800

    Restoring Developer Tools

--- index.js
+++ index.js
@@ -44,3 +44,19 @@ if(process.platform === 'darwin')
     menuTemplate.unshift({});
 }
 
+// TODO: Add Developer Tool Menu dynamically
+if(process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click: (item, focusedWindow) => {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
+
